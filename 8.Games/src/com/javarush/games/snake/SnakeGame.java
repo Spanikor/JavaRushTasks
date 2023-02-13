802e81c369d6f58221a8c48ecec6a72ca4cf4b5f
Spani <Al-93_2011@mail.ru>
2023-02-13 20:13:40 +0300

    paint game fields Game Snake part 1/20

--- 8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -7,9 +7,26 @@ public class SnakeGame extends Game {
     public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
 
+
     @Override
     public void initialize() {
-        setScreenSize(15,15);
+        setScreenSize(15, 15);
+        createGame();
+
+
+    }
+    private void createGame() {
+        drawScene();
+    }
+
+    private void drawScene() {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                setCellColor(x, y, Color.RED);
+
+            }
+
+        }
 
     }
 }
